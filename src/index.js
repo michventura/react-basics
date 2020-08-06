686a74eb2caebd5000f2867b62aa841c007a8921
Mich Ventura <michcodes@hotmail.com>
2020-08-06 12:11:11 -0500

    react fragments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-function App({name = 'Mich'}) {
-  return <p>Hola {name}</p>
-}
+const App = ({name}) => (
+  <>
+    <p>Hola {name}</p>
+  </>
+)
 
 const rootElement = document.getElementById('root')
 render(<App name="Joe" />, rootElement)
